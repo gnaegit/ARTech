cd02955c3d3e15f41ccbff6823c273d6d50624c0
Gnägi Tobias <gat5@bfh.ch>
2023-10-19 12:39:10 +0200

    several changes

--- Assets/Scripts/MachineTargetRepresentation.cs
+++ Assets/Scripts/MachineTargetRepresentation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,42 @@ using UnityEngine;
 public class MachineTargetRepresentation : MonoBehaviour
 {
     public GameObject description;
+    public GameObject arrowsToFront;
+    public GameObject arrowsToBack;
+    public GameObject frontArea;
+    public GameObject backArea;
     public MachineModel model;
 
     void Start()
     {
-        DeactivateDescription();
+        //DeactivateDescription();
+        DeactivateArrowsToBack();
+        DeactivateArrowsToFront();
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+    }
+
+    public void ActivateArrowsToFront()
+    {
+        arrowsToFront.SetActive(true);
+    }
+
+    public void DeactivateArrowsToFront()
+    {
+        arrowsToFront.SetActive(false);
+    }
+
+    public void ActivateArrowsToBack()
+    {
+        arrowsToBack.SetActive(true);
+    }
+
+    public void DeactivateArrowsToBack()
+    {
+        arrowsToBack.SetActive(false);
     }
 
     void ActivateDescription()
@@ -20,5 +52,30 @@ public class MachineTargetRepresentation : MonoBehaviour
     void DeactivateDescription()
     {
         description.SetActive(false);
+    }
+
+    public Boolean IsUserInFrontArea()
+    {
+        Collider[] hitcolliders = Physics.OverlapSphere(Camera.main.transform.position, 0f);
+        
+        foreach (Collider collider in hitcolliders) 
+        {
+            if(collider.gameObject == frontArea)
+                return true;
+        }
+
+        return false;
+    }
+    public Boolean IsUserInBackArea()
+    {
+        Collider[] hitcolliders = Physics.OverlapSphere(Camera.main.transform.position, 0f);
+
+        foreach (Collider collider in hitcolliders)
+        {
+            if (collider.gameObject == backArea)
+                return true;
+        }
+
+        return false;
     }
 }
---
+++ Assets/SetupGoToBackInstruction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SetupGoToBackInstruction : MonoBehaviour
+{
+    [System.Serializable] public class OnUserIsInBackArea : UnityEvent { }
+    [SerializeField] OnUserIsInBackArea onUserIsInBackArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MachineTargetSingleton.Instance.machineTargetRepresentation.ActivateArrowsToBack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MachineTargetSingleton.Instance.machineTargetRepresentation.IsUserInBackArea())
+        {
+            onUserIsInBackArea.Invoke();
+        }
+    }
+
+    void OnDestroy()
+    {
+        MachineTargetSingleton.Instance.machineTargetRepresentation.DeactivateArrowsToBack();
+    }
+}
