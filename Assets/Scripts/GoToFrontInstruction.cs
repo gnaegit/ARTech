b59068b2fbde456f59872fcdbeb9622ec5c3b95e
Gnägi Tobias <gat5@bfh.ch>
2023-10-26 15:34:01 +0200

    add several animations

---
+++ Assets/Scripts/GoToFrontInstruction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GoToFrontInstruction : MonoBehaviour
+{
+    [System.Serializable] public class OnUserIsInFrontArea : UnityEvent { }
+    [SerializeField] OnUserIsInFrontArea onUserIsInFrontArea;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MachineTargetSingleton.Instance.machineTargetRepresentation.ActivateArrowsToFront();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MachineTargetSingleton.Instance.machineTargetRepresentation.IsUserInFrontArea())
+        {
+            onUserIsInFrontArea.Invoke();
+        }
+    }
+
+    void OnDestroy()
+    {
+        MachineTargetSingleton.Instance.machineTargetRepresentation.DeactivateArrowsToFront();
+    }
+}
